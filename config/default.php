6fc704f09cc87e230d5b86b07de411a37a94628c
Sander Krause <sanderkrause@gmail.com>
2015-09-15 21:52:07 +0200

    Removed database configuration, as it's not needed. Also refactored array notation to use the newer short array notation instead.

--- config/default.php
+++ config/default.php
@@ -1,21 +1,8 @@
 <?php
 
-$config = array(
+$config = [
     'debug' => false,
-    'twig.options' => array(
-        'twig.path' => array(PROJECT_ROOT . 'src' . DIRECTORY_SEPARATOR . 'views'),
-        'twig.paths' => array(
-            '__main__' => array(PROJECT_ROOT . 'src' . DIRECTORY_SEPARATOR . 'views')
-        )
-    ),
-    'db.options' => array(
-        'db.options' => array(
-            'driver'   => 'pdo_mysql',
-            'dbname' => 'test',
-            'host' => '127.0.0.1',
-            'port' => 3306,
-            'user' => 'myuser',
-            'password' => 'mypass'
-        ),
-    )
-);
+    'twig.options' => [
+        'twig.path' => [APP_ROOT . 'views'],
+    ]
+];
--- config/development.php
+++ config/development.php
@@ -3,6 +3,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$environment_config = array(
+$environment_config = [
     'debug' => true,
-);
+];
